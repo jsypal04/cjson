364985b314f09999a523989d616c4a5d435b6115
jsypal04 <60202320+jsypal04@users.noreply.github.com>
2025-03-02 09:37:22 -0500

    completed the first unit test for the parser. it passed

--- tests/parserTests/parserTest1.c
+++ tests/parserTests/parserTest1.c
@@ -3,12 +3,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 #define NUM_KEYS 10
+#define NUM_VALUES 12
 
-char* correctKeys[] = {"main", "id", "inStock", "id", "inStock", "mainVendor", "CEO", "firstName", "lastName", "age"};
+char* correctKeys[] = {"\"main\"", "\"id\"", "\"inStock\"", "\"id\"", "\"inStock\"", "\"mainVendor\"", "\"CEO\"", 
+                       "\"firstName\"", "\"lastName\"", "\"age\""};
 char parsedKeys[NUM_KEYS][MAX_LEX_LEN];
-int numKeysParsed = 0;
+unsigned int numKeysParsed = 0;
+
+char* correctValues[] = {"array", "object", "0", "true", "object", "1.34", "false", "null", "object", "\"John\"", "\"Doe\"", "-34.28"};
+char parsedValues[NUM_VALUES][MAX_LEX_LEN];
+unsigned int numValuesParsed = 0;
 
 // forward declarations
 void checkObject(ObjectAST* obj);
@@ -18,9 +25,18 @@ void checkValue(ValueAST* value);
 void checkNextValue(NextValueAST* nextValue);
 
 void checkObject(ObjectAST* obj) {
+    if (numKeysParsed >= NUM_KEYS) {
+        printf("ERROR: parsed test 1 (basic syntax test) failed. Too many keys were found.\n"); 
+        exit(1);
+    }
     strcpy(parsedKeys[numKeysParsed], obj->key);
     numKeysParsed++;
 
+    if (obj->value == NULL) {
+        printf("ERROR: parser test 1 (basic syntax test) failed. The value field of the ObjectAST struct is NULL.\n");
+        exit(1);
+    }
+    
     checkValue(obj->value);
     if (obj->nextPair != NULL) {
         checkNextPair(obj->nextPair);
@@ -35,6 +51,11 @@ void checkArray(ArrayAST* array) {
 }
 
 void checkNextPair(NextPairAST* nextPair) {
+    if (numKeysParsed >= NUM_KEYS) {
+        printf("ERROR: parsed test 1 (basic syntax test) failed. Too many keys were found.\n"); 
+        exit(1);
+    }
+
     strcpy(parsedKeys[numKeysParsed], nextPair->key);
     numKeysParsed++;
     
@@ -46,10 +67,33 @@ void checkNextPair(NextPairAST* nextPair) {
 
 void checkValue(ValueAST* value) {
     if (value->obj != NULL) {
+        if (numValuesParsed >= NUM_VALUES) {
+            printf("ERROR: parser test 1 (basic syntax test) failed. Too many values were found.\n");
+            exit(1);
+        }
+
+        strcpy(parsedValues[numValuesParsed], "object");
+        numValuesParsed++;
         checkObject(value->obj);
     }
     else if (value->array != NULL) {
+        if (numValuesParsed >= NUM_VALUES) {
+            printf("ERROR: parser test 1 (basic syntax test) failed. Too many values were found.\n");
+            exit(1);
+        }
+
+        strcpy(parsedValues[numValuesParsed], "array");
+        numValuesParsed++;
         checkArray(value->array);
+    }
+    else if (value->lexeme != NULL) {
+        if (numValuesParsed >= NUM_VALUES) {
+            printf("ERROR: parser test 1 (basic syntax test) failed. Too many values were found.\n");
+            exit(1);
+        }
+
+        strcpy(parsedValues[numValuesParsed], value->lexeme);
+        numValuesParsed++;
     }
 }
 
@@ -72,9 +116,30 @@ int main(int argc, char* argv[]) {
     printf("\n------------------------------------\n");
 
     checkObject(ast);
+
+    bool passed = true;
     for (int i = 0; i < NUM_KEYS; i++) {
-        printf("Parsed Key: %s\nCorrect Key: %s\n\n", parsedKeys[i], correctKeys[i]);
+        if (strcmp(parsedKeys[i], correctKeys[i])) {
+            passed = false;
+            printf(
+                "ERROR: parser test 1 (basic syntax test) failed. Parsed key %s and correct key %s do not match.\n", 
+                parsedKeys[i], correctKeys[i]
+            );
+        }
+    }
+    for (int i = 0; i < NUM_VALUES; i++) {
+        if (strcmp(parsedValues[i], correctValues[i])) {
+            passed = false;
+            printf(
+                "ERROR: parser test 1 (basic syntax test) failed. Parsed key %s and correct key %s do not match.\n", 
+                parsedValues[i], correctValues[i]
+            );
+        }
     }
 
-    exit(0);
+    if (passed) {
+        printf("Parser test 1 passed.\n");
+        exit(0);
+    }
+    exit(1);
 }
